5a88f4fe3bc5b41053c485f9f9a078ac61993035
Thiago <thaguilardev@gmail.com>
2025-08-05 19:51:27 -0300

    JS da barra de length

--- src/components/PasswordOptions.jsx
+++ src/components/PasswordOptions.jsx
@@ -1,19 +1,61 @@
 import "./PasswordOptions.css";
 import IncrementButton from "./IncrementButton.jsx";
+import { useEffect, useState } from "react";
 
 function PasswordOptions() {
+  // --- DEFINIÇÃO DE useStates
+
+  const [passwordLenght, setPasswordLenght] = useState(10);
+  const [rangeBarBg, setRangeBarBg] = useState("");
+
+  // FUNÇÃO GLOBAL para o "rangeBar" => ( cada elemento so pode receber 1 função Onclick)
+  const onRangeBarChange = (e) => {
+    const rangeBar = e.target;
+    const value = rangeBar.value;
+
+    // atualização do SPAN INDICADOR DO LENGHT
+    setPasswordLenght(value);
+
+    // atualização da COR DE FUNDO DA BARRA de acordo com o valor => Usando o linear gradient
+    const min = rangeBar.min;
+    const max = rangeBar.max;
+    const porcent = ((value - min) / (max - min)) * 100;
+
+    const fillColor = "hsl(127, 44%, 62%)";
+    const bgColor = "hsl(248, 15%, 11%)";
+
+    const novoBackground = `linear-gradient(to right, ${fillColor} ${porcent}%, ${bgColor} ${porcent}%)`;
+
+    setRangeBarBg(novoBackground);
+  };
+
+  // useEffect = hook do react para realizar/gerenciar operações que não são totalmente reativas, especificando quando uma ação deve ser usada
+  // nesse caso para executar a lógica apenas uma vez quando o componente carrega
+  useEffect(() => {
+    // criação de um evento falso para passar inicialmente a função de mudança do rangeBar
+    const fakeEvent = { target: { value: passwordLenght, min: 0, max: 20 } };
+    onRangeBarChange(fakeEvent);
+  }),
+    // esse segundo argumento é responsavel pela "lista de gatilhos" para executar novamente o useEffect, como so precisa inicialmente,  utiliza-se a string vazia
+    [];
+
   return (
     <div className="password-options">
       <div className="password-lenght">
         <div className="top-box">
-          <label htmlFor="iPasswordLenght">Character Lenght</label>
-          <span>0</span>
+          <label htmlFor="iPasswordLenght">Character Length</label>
+          <span>{passwordLenght}</span>
         </div>
         <form>
           <input
             type="range"
             min={0}
             max={20}
+            value={passwordLenght}
+            // REFERENCIA DA FUNÇÃO = Log de evento + complexa // ARROW FUNCTION INLINE = Log de evento simples + curta
+            // exemp: onChange = setPasswordLenght
+            onChange={onRangeBarChange}
+            style={{ background: rangeBarBg }}
             name="passwordLenght"
             id="iPasswordLenght"
             className="password-range-lenght"
@@ -24,8 +66,8 @@ function PasswordOptions() {
       <div className="password-increment-options">
         <IncrementButton incrementElement="Uppercase Letters" />
         <IncrementButton incrementElement="Lowercase Letters" />
-        <IncrementButton incrementElement="Numbers Letters" />
-        <IncrementButton incrementElement="Symbols Letters" />
+        <IncrementButton incrementElement="Numbers" />
+        <IncrementButton incrementElement="Symbols" />
       </div>
 
       <div className="password-strength">
